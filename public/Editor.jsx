7fb982c1f8cad410b974553ef2e9ba5f3d52fa63
Yokubjon-J <ezenxaanmeiji@gmail.com>
2023-09-07 23:59:18 +0500

    editor enhancement, needs a margin between extensions

--- public/Editor.jsx
+++ public/Editor.jsx
@@ -1,45 +1,57 @@
 import remirrorEditorStyles from 'remirror/styles/all.css';
 // import 'remirror/styles/all.css'; //this is from remirror docs. But it won't work. The above is a part of a (temporary) hack.
-
-import { BoldExtension } from 'remirror/extensions';
-import { EditorComponent, Remirror, useRemirror, useActive, useChainedCommands } from '@remirror/react';
+import { useCommands } from '@remirror/react';
+import { htmlToProsemirrorNode } from 'remirror';
+import { BoldExtension, ItalicExtension } from 'remirror/extensions';
+import { EditorComponent,
+    Remirror, 
+    ThemeProvider, 
+    ToggleItalicButton, 
+    ToggleBoldButton,
+    Toolbar,
+    useRemirror 
+  } from '@remirror/react';
 
 const Menu = () => {
-  console.log('this is a hack in order to get the styles injected: ', remirrorEditorStyles);
-    // Using command chaining
-    const chain = useChainedCommands();
-    const active = useActive();
-  
-    return (
-      <button
-        onClick={() => {
-          chain // Begin a chain
-            .toggleBold()
-            .focus()
-            .run(); // A chain must always be terminated with `.run()`
-        }}
-        style={{ fontWeight: active.bold() ? 'bold' : undefined }}
-      >
-        B
-      </button>
-    );
-  };
+    console.log('this is a hack in order to get the styles injected: ', remirrorEditorStyles);
+        const { toggleBold, focus } = useCommands();
+        
+        return (
+            <button
+                onClick={() => {
+                    toggleBold();
+                    focus();
+                }}
+                style={{ fontWeight: active.bold() ? 'bold' : undefined }}
+                onClick={() => toggleBold()} disabled={toggleBold.enabled() === false}
+            >
+                B
+            </button>
+        );
+    };
 
 export default function MyApp() {
-  const { manager, state } = useRemirror({
-    extensions: () => [new BoldExtension()],
-    content: '<p>I love <b>Remirror</b></p>',
-    selection: 'start',
-    stringHandler: 'html',
-  });
-
-  return (
-    <div className='remirror-theme'>
-      <Remirror manager={manager} initialContent={state}>
-        {/* The text editor is placed above the menu to make the zIndex easier to manage for popups */}
-        <EditorComponent />
-        <Menu />
-      </Remirror>
-    </div>
-  );
+    const { manager, state, onChange } = useRemirror({
+        extensions: () => [new BoldExtension(), new ItalicExtension()],
+        content: '<p>I love <b>Remirror</b></p>',
+        selection: 'start',
+        stringHandler: htmlToProsemirrorNode,
+    });
+  
+    return (
+        <ThemeProvider>
+            <Remirror
+                manager={manager}
+                autoFocus
+                onChange={onChange}
+                initialContent={state}
+                autoRender='end'
+            >
+                <Toolbar>
+                    <ToggleItalicButton />
+                    <ToggleBoldButton />
+                </Toolbar>
+            </Remirror>
+        </ThemeProvider>
+    );
 };
